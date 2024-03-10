6f460408f37f7297d5dff50b4e8cdc5dadf6e55e
zdy-hnu <97009404+zdy-hnu@users.noreply.github.com>
2024-03-10 12:00:19 +0800

    完成基本收支记录功能
    但是，缺乏余额<0时的正确逻辑（待改进）

--- 01account/Account.java
+++ 01account/Account.java
@@ -6,7 +6,11 @@ class Account
 {
 	public static void main(String[] args) 
 	{
-		boolean isFlag = true;
+		boolean isFlag = true;//控制循环结束
+
+		int money = 0;//当前金额
+
+		String info = "";//收支说明
 		
 		while(isFlag)
 		{
@@ -22,13 +26,40 @@ class Account
 			switch(selection)
 			{
 				case '1':
-					System.out.println("                   1 收支明细");
+					System.out.println("-----------------当前收支明细记录-----------------");
+					System.out.println("收支\t账户金额\t收支金额\t说    明");
+					System.out.println(info);
+					System.out.println("--------------------------------------------------");
 					break;
 				case '2':
-					System.out.println("                   2 收支明细");
+					System.out.print("本次收入金额：");
+					int income = Utility.readNumber();
+
+					if(income > 0){
+						money += income;
+					}
+
+					System.out.print("本次收入说明：");
+					String inDescription = Utility.readString();
+
+					info += "收入\t" + money + "\t\t" + income + "\t\t" + inDescription + "\n";
+
+					System.out.println("---------------------登记完成---------------------");
 					break;
 				case '3':
-					System.out.println("                   3 收支明细");
+					System.out.print("本次支出金额：");
+					int outcome = Utility.readNumber();
+
+					if(outcome > 0 && money >= outcome){
+						money -= outcome;
+					}
+
+					System.out.print("本次支出说明：");
+					String outDescription = Utility.readString();
+
+					info += "收出\t" + money + "\t\t" + outcome + "\t\t" + outDescription + "\n";
+
+					System.out.println("---------------------登记完成---------------------");
 					break;
 				case '4':
 					System.out.println("确认是否退出(Y/N)：");
